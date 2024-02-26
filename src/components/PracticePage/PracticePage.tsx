cfdcac4cf317821a7535b0c0956224152d69f200
rgsk <guptarahul70322@gmail.com>
2024-02-26 18:23:35 +0530

    done

--- src/components/PracticePage/PracticePage.tsx
+++ src/components/PracticePage/PracticePage.tsx
@@ -1,10 +1,19 @@
-import { Button } from "../ui/button";
+import { ModeToggle } from "../ModeToggle";
+import { Button } from "../ui/moving-border";
+import { TextGenerateEffect } from "../ui/text-generate-effect";
 
 interface PracticePageProps {}
 const PracticePage: React.FC<PracticePageProps> = ({}) => {
   return (
     <div>
-      <Button>Practice</Button>
+      <ModeToggle />
+      <TextGenerateEffect words="Hii, How are you?" />
+      <Button
+        borderRadius="1.75rem"
+        className="bg-white dark:bg-slate-900 text-black dark:text-white border-neutral-200 dark:border-slate-800"
+      >
+        Borders are cool
+      </Button>
     </div>
   );
 };
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
+import { ThemeProvider } from "@/providers/theme-provider";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+    >
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
