4a9b4affdd9e06d1da7c4784cfd2b360d966b778
rgsk <guptarahul70322@gmail.com>
2024-02-26 18:12:27 +0530

    shadcdn setup

---
+++ src/components/PracticePage/PracticePage.tsx
@@ -0,0 +1,11 @@
+import { Button } from "../ui/button";
+
+interface PracticePageProps {}
+const PracticePage: React.FC<PracticePageProps> = ({}) => {
+  return (
+    <div>
+      <Button>Practice</Button>
+    </div>
+  );
+};
+export default PracticePage;
