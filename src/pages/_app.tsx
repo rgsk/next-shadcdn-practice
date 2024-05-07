19ec2851a4dcc0bcdafa91745e637a473a2a4d4c
rgsk <guptarahul70322@gmail.com>
2024-05-07 18:03:53 +0530

    render code snippets

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,10 @@
 import { ThemeProvider } from "@/providers/theme-provider";
 import "@/styles/globals.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import "highlight.js/styles/vs2015.css";
 import type { AppProps } from "next/app";
 import "react-resizable/css/styles.css";
+
 const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
---
+++ src/utils/html.ts
@@ -0,0 +1,10 @@
+export default function html(strings: any, ...values: any) {
+  let result = "";
+  for (let i = 0; i < strings.length; i++) {
+    result += strings[i];
+    if (i < values.length) {
+      result += values[i];
+    }
+  }
+  return result;
+}
