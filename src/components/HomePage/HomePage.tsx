69dc751a7afd386c50645937fe5b7cae75bc34c2
rgsk <guptarahul70322@gmail.com>
2024-05-12 02:42:22 +0530

    done

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -1,13 +1,19 @@
+import { useTheme } from "next-themes";
 import Link from "next/link";
+import { useEffect } from "react";
+import { Button } from "../ui/button";
 
 interface HomePageProps {}
 const HomePage: React.FC<HomePageProps> = ({}) => {
+  const { setTheme } = useTheme();
+  useEffect(() => {
+    setTheme("light");
+  }, [setTheme]);
   return (
-    <div className="flex flex-col gap-4">
-      <Link href="/navigate-image">Navigate Image</Link>
-      <Link href="/transcribe-text">Transcribe Text</Link>
-      <Link href="/text-to-speech-example">Text To Speech Example</Link>
-      <Link href="/chat">Chat</Link>
+    <div className="flex flex-col gap-4 p-4">
+      <Link href="/answer-evaluator">
+        <Button>Answer Evaluator</Button>
+      </Link>
     </div>
   );
 };
